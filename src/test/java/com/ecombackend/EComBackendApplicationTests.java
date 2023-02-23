74075a25ed5b05b693f593f86e798cd7d19b7d80
bishalchaurasiya <bishal.chaurasiya618@gmail.com>
2023-02-23 15:52:28 +0530

    second_commit

---
+++ src/test/java/com/ecombackend/EComBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
