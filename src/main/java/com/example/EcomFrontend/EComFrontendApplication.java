0dd7553662f48b5631d366c9b8423c198d6dab6f
nisha kothare <53811249+nisha555@users.noreply.github.com>
2023-02-23 15:53:01 +0530

    my first code

---
+++ src/main/java/com/example/EcomFrontend/EComFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcomFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComFrontendApplication.class, args);
+	}
+
+}
